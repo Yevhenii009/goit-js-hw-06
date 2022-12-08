0f062407bef5dfdadd3bf95b3c8eaead6f80d31d
Yevhenii009 <stifgun@gmail.com>
2022-12-08 15:16:28 +0200

    дз10

--- js/task-10.js
+++ js/task-10.js
@@ -1,22 +1,22 @@
-const form = document.querySelector('input');
-const createBtn = document.querySelector('#controls button');
+// const form = document.querySelector('input');
+// const createBtn = document.querySelector('#controls button');
 
 
-const newDiv = document.createElement('div');
+// const newDiv = document.createElement('div');
 
 
-function createBoxes(amount) {
-newDiv = form
-}
-
-
-
+// function createBoxes(amount) {
+// newDiv = form
+// }
 
 
 
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+
+
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
 
 
